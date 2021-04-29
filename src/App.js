2916eb0026cfc6fe1ee5c9773199c4bb94d2a878
kentwalters <kentwaltersmail@gmail.com>
2021-04-28 23:01:01 -0700

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Retool from './components/Retool'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Retool url="https://retoolin.tryretool.com/embedded/public/f7607e1f-670a-4ebf-9a09-be54cf17181e"/>
   );
 }
 
---
+++ src/components/Retool.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import './retool.css';
+
+class Retool extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            url: props.url,
+            elementWatchers: {}
+        }
+    }
+
+    componentDidMount(){
+        this.startListening()
+        this.startWatchers()
+    }
+
+    startListening = () => {
+        window.addEventListener('message', (e) => this.handle(e) );
+    }
+
+    startWatchers = () => {
+        var watcherKeys = Object.keys(this.state.elementWatchers)
+
+        for (var i = 0; i < watcherKeys.length; i++) {
+            var key = watcherKeys[i]
+            var watcher = this.state.elementWatchers[key]
+            var selector = watcher.selector
+            var node = document.querySelector(selector)
+            var value = node?.textContent
+            if (value !== watcher.prevValue) {
+                watcher.prevValue = value
+                watcher.iframe.contentWindow.postMessage(
+                {
+                    type: 'PARENT_WINDOW_RESULT',
+                    result: value,
+                    id: watcher.queryId,
+                    pageName: watcher.pageName,
+                },
+                '*',
+                )
+            }
+        }
+
+        setTimeout(this.startWatchers, 100)  
+    }
+
+    createOrReplaceWatcher = (selector, pageName, queryId) => {
+        var watcherId = pageName + '-' + queryId
+        var watchers = {...this.state.elementWatchers}
+
+        watchers[watcherId] = {
+            iframe: this.iframe,
+            selector: selector,
+            pageName: pageName,
+            queryId: queryId,
+            prevValue: null,
+        }
+
+        this.setState({elementWatchers: watchers})
+    }
+
+    handle = (event) => {
+        if (!this.iframe.contentWindow) return
+        
+        var node;
+    
+        if (event.data.type === 'PARENT_WINDOW_QUERY') {
+            node = document.querySelector(event.data.selector)
+            this.createOrReplaceWatcher(event.data.selector, event.data.pageName, event.data.id)
+
+            this.iframe.contentWindow.postMessage(
+                {
+                    type: 'PARENT_WINDOW_RESULT',
+                    result: node?.textContent,
+                    id: event.data.id,
+                    pageName: event.data.pageName,
+                },
+                '*',
+            )
+        }
+
+        if (event.data.type === 'PARENT_WINDOW_PREVIEW_QUERY') {
+            node = document.querySelector(event.data.selector)
+            this.iframe.contentWindow.postMessage(
+                {
+                    type: 'PARENT_WINDOW_PREVIEW_RESULT',
+                    result: node?.textContent,
+                    id: event.data.id,
+                },
+                '*',
+            )
+        }       
+    
+    }
+
+    render() {
+        return (
+            <iframe 
+                frameBorder="none" 
+                src={this.state.url}
+                ref = {e => {
+                    this.iframe = e
+                }}
+            >
+            </iframe>
+        );
+    }
+}
+  
+  export default Retool;
+  

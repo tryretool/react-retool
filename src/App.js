ad1979af089d836b5d6904efae7feb2beda51635
kentwalters <kentwaltersmail@gmail.com>
2021-08-12 20:22:11 -0700

    Revmove comment.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Retool from './components/Retool'
 
 function App() {
   return (
-    // <Retool/>
     <Retool url="https://retoolin.tryretool.com/embedded/public/f7607e1f-670a-4ebf-9a09-be54cf17181e"></Retool>
   );
 }
